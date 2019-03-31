95e0ea36f854a0ac3cdba87e9d605b90247e87a1
Adrian <adro_kr@hotmail.com>
2019-03-31 15:16:49 -0400

    first commit

---
+++ src/aws/api/client/Test1.java
@@ -0,0 +1,79 @@
+package aws.api.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.BoundingBox;
+import com.amazonaws.services.rekognition.model.CompareFacesMatch;
+import com.amazonaws.services.rekognition.model.CompareFacesRequest;
+import com.amazonaws.services.rekognition.model.CompareFacesResult;
+import com.amazonaws.services.rekognition.model.ComparedFace;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.util.IOUtils;
+
+public class Test1 {
+	public static void main(String[] args) throws InterruptedException {
+		Float similarityThreshold = 80F;
+		String sourceImage = "C:/Users/acortes/Desktop/img/me.jpg";
+		String targetImage = "C:/Users/acortes/Desktop/img/me.jpg";
+		ByteBuffer sourceImageBytes = null;
+		ByteBuffer targetImageBytes = null;
+
+		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
+
+		// Load source and target images and create input parameters
+		try (InputStream inputStream = new FileInputStream(new File(sourceImage))) {
+			sourceImageBytes = ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
+		} catch (Exception e) {
+			System.out.println("Failed to load source image " + sourceImage);
+			System.exit(1);
+		}
+		try (InputStream inputStream = new FileInputStream(new File(targetImage))) {
+			targetImageBytes = ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
+		} catch (Exception e) {
+			System.out.println("Failed to load target images: " + targetImage);
+			System.exit(1);
+		}
+
+		Image source = new Image().withBytes(sourceImageBytes);
+		Image target = new Image().withBytes(targetImageBytes);
+
+		CompareFacesRequest request = new CompareFacesRequest().withSourceImage(source).withTargetImage(target)
+				.withSimilarityThreshold(similarityThreshold);
+
+		// Call operation
+		
+		
+		for (int i = 0; i < 10; i++) {
+			long startTime = System.currentTimeMillis();
+			CompareFacesResult compareFacesResult = rekognitionClient.compareFaces(request);
+			long endTime   = System.currentTimeMillis();
+			long totalTime = endTime - startTime;
+			System.out.println("REQUEST TIME: " +totalTime);
+
+			// Display results
+			List<CompareFacesMatch> faceDetails = compareFacesResult.getFaceMatches();
+			for (CompareFacesMatch match : faceDetails) {
+				ComparedFace face = match.getFace();
+				BoundingBox position = face.getBoundingBox();
+				System.out.println("Face at " + position.getLeft().toString() + " " + position.getTop()
+						+ " matches with " + face.getConfidence().toString() + "% confidence.");
+
+			}
+		
+		List<ComparedFace> uncompared = compareFacesResult.getUnmatchedFaces();
+
+		System.out.println("There was " + uncompared.size() + " face(s) that did not match");
+		System.out.println("Source image rotation: " + compareFacesResult.getSourceImageOrientationCorrection());
+		
+		
+		Thread.sleep(333);
+		
+		}
+	}
+}
